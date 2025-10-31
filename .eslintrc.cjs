c1926414a120fcbfd9013a2ff308b8cc62c7fa4f
CodeDeX <splashdexstudios@gmail.com>
2025-10-31 02:36:50 +0000

    chore: checkpoint before auto-fix (Phase 3)

---
+++ .eslintrc.cjs
@@ -0,0 +1,50 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier', // MUST be last to override other configs
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module', // ESM modules
+  },
+  plugins: [
+    '@typescript-eslint',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': ['error', {
+      singleQuote: true,
+      semi: true,
+      trailingComma: 'es5',
+    }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', {
+      argsIgnorePattern: '^_',
+      varsIgnorePattern: '^_'
+    }],
+  },
+  overrides: [
+    {
+      // Jest test files configuration
+      files: ['**/__tests__/**/*', '**/*.test.js', '**/*.test.ts', '**/*.spec.js'],
+      plugins: ['jest'],
+      env: {
+        'jest/globals': true,  // Enables Jest globals (describe, it, expect)
+      },
+      extends: ['plugin:jest/recommended'],
+      rules: {
+        'jest/no-disabled-tests': 'warn',
+        'jest/no-focused-tests': 'error',
+        'jest/valid-expect': 'error',
+      },
+    },
+  ],
+};
